36198a93968c2d1e554d261f7a9c1a859c55664b
sgyuebing <108126059+sgyuebing@users.noreply.github.com>
2025-05-05 04:29:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,8 @@
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git Aibx https://github.com/Aibx/openwrt-packages' feeds.conf.default
 sed -i '$a src-git immortalwrt https://github.com/immortalwrt/packages' feeds.conf.default
-#sed -i '$a src-git mmdvm https://github.com/sgyuebing/mmdvm-openwrt' feeds.conf.default
-sed -i '$a src-git mmdvm https://github.com/lazywalker/mmdvm-openwrt' feeds.conf.default
-sed -i '$a src-git devtools https://github.com/lazywalker/devtools-feeds' feeds.conf.default
+sed -i '$a src-git netwizard https://github.com/sirpdboy/luci-app-netwizard' feeds.conf.defaul
+sed -i '$a src-git mmdvm https://github.com/qzyuebing/mmdvm-openwrt' feeds.conf.default
+#sed -i '$a src-git mmdvm https://github.com/lazywalker/mmdvm-openwrt' feeds.conf.default
+sed -i '$a src-git pptp https://github.com/qzyuebing/luci-app-pptp-server' feeds.conf.default
+sed -i '$a src-git modem https://github.com/FUjr/modem_feeds' feeds.conf.default
